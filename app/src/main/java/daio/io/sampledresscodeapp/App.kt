abe8bc92154584bcf9940a274626fc3fdca2aa7c
Ye Thu <yethu.personal@gmail.com>
2018-09-01 00:44:35 +0630

    Updated declaration according to new implementation

--- app/src/main/java/daio/io/sampledresscodeapp/App.kt
+++ app/src/main/java/daio/io/sampledresscodeapp/App.kt
@@ -1,17 +1,15 @@
 package daio.io.sampledresscodeapp
 
 import android.app.Application
-import daio.io.dresscode.DressCode
 import daio.io.dresscode.declareDressCode
 
 class App : Application() {
-
     override fun onCreate() {
         super.onCreate()
 
         declareDressCode(
-            DressCode("ThemeOne", R.style.ThemeOne),
-            DressCode("ThemeTwo", R.style.ThemeTwo)
+                R.style.ThemeOne,
+                R.style.ThemeTwo
         )
     }
 }
